3ce5d3bc528701fb975b2c178960daceed87e360
Teodor Stoyanov <teo.stoyanov1@gmail.com>
2019-11-28 11:41:16 +0200

    Change class name and class method

--- src/main/java/orgprimeholding/core/InitializeDb.java
+++ src/main/java/orgprimeholding/core/InitializeDb.java
@@ -6,11 +6,11 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
-public class CreateTables {
-    private CreateTables() {
+public class InitializeDb {
+    private InitializeDb() {
     }
 
-    public static void createMyDb(Connection connection) throws SQLException {
+    public static void createTables(Connection connection) throws SQLException {
         for (String query : SqlQueries.getAllQueries()) {
             PreparedStatement preparedStatement = connection.prepareStatement(query);
             preparedStatement.execute();
